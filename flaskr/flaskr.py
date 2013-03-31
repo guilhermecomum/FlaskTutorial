552c963e2db4a5086016b5d2b6fb3acee7e318fc
Guilherme Guerra <guerrinha@comum.org>
2013-03-30 23:20:01 -0300

    ending setup

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -6,6 +6,16 @@ from flask import Flask, request, session, g, redirect, url_for, \
 # configuration
 DATABASE = '/tmp/flaskr.db'
 DEBUG = True
-SECRET_KEY = 'development key'
+SECRET_KEY = 'f1dc3cf82aeb6c893805ec17ed60b3b8'
 USERNAME = 'admin'
 PASSWORD = 'default'
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+if __name__ == '__main__':
+    app.run()
