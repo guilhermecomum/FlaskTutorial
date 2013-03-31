e5f5a62105714b9db57c1c7d6ee6835e1a03e18f
Guilherme Guerra <guerrinha@comum.org>
2013-03-31 15:35:00 -0300

    Yes! We have a database!

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
+
 # all the imports
 import sqlite3
+from contextlib import closing
+
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+
+
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
@@ -13,9 +19,27 @@ PASSWORD = 'default'
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
+
+
 if __name__ == '__main__':
+    init_db()
     app.run()
